a57885685f3b1f0b6234a83b6b55cf1255462ab1
kimi_no_shiriai <94500383+C4se-K@users.noreply.github.com>
2025-01-24 17:21:13 -0800

    250124

---
+++ main.py
@@ -0,0 +1,28 @@
+import datetime
+import math
+import numpy as np
+
+
+
+class entitiy:
+    def __init__(self):
+        pass
+
+    def decay(self):
+        pass
+
+    def evaluate(self):
+        pass
+
+class simulator:
+    def __init__(self):
+        pass
+
+    def add(self):
+        pass
+
+    def decay(self):
+        pass
+
+    def process(self):
+        pass
